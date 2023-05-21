a1d6d5eafa87e98ff81d78a9ebe57559d5d5d1ad
sofia <m.sofia.a.campos@gmail.com>
2023-05-21 13:32:28 -0400

    primer código

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,2 +1,29 @@
-let numeros = [1,2,3,4,5,6,7];
- console.log(numeros)
+// Obtener los elementos de la calculadora
+const display = document.getElementById('display');
+const buttons = Array.from(document.getElementsByClassName('btn'));
+
+// Asignar el evento click a cada botón
+buttons.forEach(button => {
+  button.addEventListener('click', () => {
+    // Obtener el valor del botón clickeado
+    const value = button.getAttribute('data-value');
+    
+    // Realizar la acción correspondiente al valor del botón
+    switch(value) {
+      case 'ac':
+        display.textContent = '';
+        break;
+      case '=':
+        try {
+          display.textContent = eval(display.textContent);
+        } catch {
+          display.textContent = 'Error';
+        }
+        break;
+      default:
+        display.textContent += value;
+        break;
+    }
+  });
+});
+
