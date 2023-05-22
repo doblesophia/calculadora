1ac75852559e265b106976c72392ba8c6e9f0592
sofia <m.sofia.a.campos@gmail.com>
2023-05-22 17:58:17 -0400

    Calculadora código JS sin el ciclo if

--- Calculadora/calculadora.js
+++ Calculadora/calculadora.js
@@ -1,29 +1,10 @@
 // Obtener los elementos de la calculadora
-const display = document.getElementById('display');
-const buttons = Array.from(document.getElementsByClassName('btn'));
+const display = document.getElementById("display");
+const buttons = Array.from(document.getElementsByClassName("btn"));
 
-// Asignar el evento click a cada botón
-buttons.forEach(button => {
-  button.addEventListener('click', () => {
-    // Obtener el valor del botón clickeado
-    const value = button.getAttribute('data-value');
-    
-    // Realizar la acción correspondiente al valor del botón
-    switch(value) {
-      case 'ac':
-        display.textContent = '';
-        break;
-      case '=':
-        try {
-          display.textContent = eval(display.textContent);
-        } catch {
-          display.textContent = 'Error';
-        }
-        break;
-      default:
-        display.textContent += value;
-        break;
-    }
-  });
-});
-
+//Asignar el click a los botones
+buttons.forEach( button => {
+  button.addEventListener("click", () =>{
+    if 
+  })
+})
